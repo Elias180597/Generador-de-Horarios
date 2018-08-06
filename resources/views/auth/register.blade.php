0abeb3a808dc52185cf4e68c7455d338ecf11bf5
ghostx94 <ierubiot@gmail.com>
2018-08-06 14:52:10 -0500

    nuevo master

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Registrarse.') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('NOMBRE:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="id" class="col-md-4 col-form-label text-md-right">{{ __('CORREO:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="id" type="id" class="form-control{{ $errors->has('id') ? ' is-invalid' : '' }}" name="id" value="{{ old('id') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('CONTRASEÑA:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('CONFIRMAR CONTRASEÑA:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="carrera" class="col-md-4 col-form-label text-md-right">{{ __('CARRERA:') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="carrera" class="form-control{{ $errors->has('carrera') ? ' is-invalid' : '' }}" name="carrera" required value="{{ old('carrera') }}">
+                                    <option value="Inenieria en Tecnologias de la Informacion">Ingenieria en Tecnologias de la Informacion</option>
+                                    <option value="Inenieria en  Energia">Ingenieria en  Energia</option>
+                                    <option value="Inenieria en Telecomunicaciones">Ingenieria en Telecomunicaciones</option>
+                                    <option value="Ingenieria en Industrial">Ingenieria en Industrial</option>
+                                </select>
+
+                                @if ($errors->has('carrera'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('carrera') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
